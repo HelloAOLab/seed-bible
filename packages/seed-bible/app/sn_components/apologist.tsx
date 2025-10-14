db72d07f33713ca221830d089d8cb0ec1c295f22
Kallyn Gowdy <kallyngowdy@gmail.com>
2025-10-14 13:09:40 -0400

    Only display the image in the preview with a "Learn More" link

--- packages/seed-bible/app/sn_components/apologist.tsx
+++ packages/seed-bible/app/sn_components/apologist.tsx
@@ -76,7 +76,7 @@ function SgCard({ item, isOpen, onToggle, viewMode = "list" }) {
     };
 
     const embUrl = toEmbeddableUrl(item);
-    const canPreview = false; //Boolean(embUrl);
+    const canPreview = !!item.image_url; //Boolean(embUrl);
 
     // short description (whatever exists in payload)
     const desc = item.description || item.summary || item.snippet || item.excerpt || "";
@@ -127,7 +127,6 @@ function SgCard({ item, isOpen, onToggle, viewMode = "list" }) {
                             <svg width="16" height="16" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M1 12C0.733333 12 0.5 11.9 0.3 11.7C0.1 11.5 0 11.2667 0 11V1C0 0.733333 0.1 0.5 0.3 0.3C0.5 0.1 0.733333 0 1 0H5.65V1H1V11H11V6.35H12V11C12 11.2667 11.9 11.5 11.7 11.7C11.5 11.9 11.2667 12 11 12H1ZM4.36667 8.35L3.66667 7.63333L10.3 1H6.65V0H12V5.35H11V1.71667L4.36667 8.35Z" fill="#859E3B" />
                             </svg>
-
                         </a>
                     )}
                 </div>
@@ -177,16 +176,19 @@ function SgCard({ item, isOpen, onToggle, viewMode = "list" }) {
             >
                 {isOpen && canPreview && (
                     <div className="sg-preview" ref={(n) => (previewRef.el = n)}>
-                        <div className="sg-iframeWrap">
-                            <div className="sg-iframeBox">
-                                <iframe
-                                    key={frameKey}
-                                    src={embUrl}
-                                    title={item.title || `preview-${item.id}`}
-                                    loading="lazy"
-                                />
-                            </div>
+                        <div className="sg-previewImgContainer">
+                            <img className="sg-previewImg" src={item.image_url} alt={item.title || `preview-${item.id}`} />
                         </div>
+                        {url && <a href={url} className="sg-preview-learnMoreLink" target="_blank"
+                            rel="noopener noreferrer"
+                            referrerPolicy="no-referrer"
+                            title="Open in new tab"
+                            aria-label="Open in new tab">
+                                Learn More
+                                <svg width="16" height="16" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1 12C0.733333 12 0.5 11.9 0.3 11.7C0.1 11.5 0 11.2667 0 11V1C0 0.733333 0.1 0.5 0.3 0.3C0.5 0.1 0.733333 0 1 0H5.65V1H1V11H11V6.35H12V11C12 11.2667 11.9 11.5 11.7 11.7C11.5 11.9 11.2667 12 11 12H1ZM4.36667 8.35L3.66667 7.63333L10.3 1H6.65V0H12V5.35H11V1.71667L4.36667 8.35Z" fill="#859E3B" />
+                                </svg>
+                            </a>}
                     </div>
                 )}
             </div>
