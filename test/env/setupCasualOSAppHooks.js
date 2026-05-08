a874a0807c78a16f6ee027dc0148a8f4c3427160
Kallyn Gowdy <kallyngowdy@gmail.com>
2026-05-08 12:20:52 -0400

    fix: Broken tests

--- test/env/setupCasualOSAppHooks.js
+++ test/env/setupCasualOSAppHooks.js
@@ -1,6 +1,14 @@
 import * as appHooks from "preact/hooks";
 import { render } from "preact";
 
+if (!globalThis.ResizeObserver) {
+  globalThis.ResizeObserver = class ResizeObserver {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  };
+}
+
 if (!globalThis.os) {
   globalThis.os = {};
 }
