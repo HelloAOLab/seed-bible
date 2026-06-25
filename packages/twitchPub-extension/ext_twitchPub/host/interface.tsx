96fb77eee05f1a3bee28cd079f7650c7b8b90d9f
Kallyn Gowdy <kallyngowdy@gmail.com>
2026-06-25 15:29:35 -0400

    fix: Linting error

--- packages/twitchPub-extension/ext_twitchPub/host/interface.tsx
+++ packages/twitchPub-extension/ext_twitchPub/host/interface.tsx
@@ -1,6 +1,5 @@
 import { type Signal } from "@preact/signals";
 import { type SeedBibleState } from "seed-bible";
-import { type I18nManager } from "seed-bible/i18n";
 
 interface IconProps {
   width?: number | string;
