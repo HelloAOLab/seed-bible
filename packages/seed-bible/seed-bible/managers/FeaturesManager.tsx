5546aea3c410f3fbcc7dff469c06721285cb9089
Kallyn Gowdy <kallyngowdy@gmail.com>
2026-07-10 12:55:21 -0400

    fix: An issue where the "currently playing" indicator was always showing during builds

--- packages/seed-bible/seed-bible/managers/FeaturesManager.tsx
+++ packages/seed-bible/seed-bible/managers/FeaturesManager.tsx
@@ -19,7 +19,7 @@ export function createFeaturesManager(posthog: PostHog | null) {
       return true;
     }
 
-    return posthog.isFeatureEnabled(featureKey);
+    return posthog.isFeatureEnabled(featureKey) ?? false;
   };
 
   return {
