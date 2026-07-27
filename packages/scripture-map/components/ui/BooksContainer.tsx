e99a181790cbb851c054291d15482db182a2a8ae
kushagra <hykukku@gmail.com>
2026-07-27 13:06:39 +0530

    Added boolean to toggle mansory layout

--- packages/scripture-map/components/ui/BooksContainer.tsx
+++ packages/scripture-map/components/ui/BooksContainer.tsx
@@ -2,8 +2,19 @@ import { memo } from "preact/compat";
 
 export interface BooksContainerProps {
   children: React.ReactNode;
+  masonry?: boolean;
 }
 
-export const BooksContainer = memo(({ children }: BooksContainerProps) => {
-  return <div className="scripture-map-books-container">{children}</div>;
-});
+export const BooksContainer = memo(
+  ({ children, masonry = true }: BooksContainerProps) => {
+    return (
+      <div
+        className={`scripture-map-books-container${
+          masonry ? " scripture-map-books-container-masonry" : ""
+        }`}
+      >
+        {children}
+      </div>
+    );
+  }
+);
